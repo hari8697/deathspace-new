2880bf6d448558f5fe0b91f97f886c911215d536
hari8697 <deathspacedesign@gmail.com>
2020-10-03 23:06:55 +0530

    feat: Add particles.js

--- src/components/sections/section1.js
+++ src/components/sections/section1.js
@@ -4,10 +4,13 @@ import {
     useViewportScroll,
     useTransform,
     useCycle,
+    AnimatePresence,
 } from "framer-motion"
 import { window } from "browser-monads"
+import Particles from "react-particles-js"
 
 //static files
+import particlesJSON from "../../assets/particles.json"
 import "../../styles/components/section1.scss"
 import logo from "../../images/Deathspace-logo-white.svg"
 
@@ -23,37 +26,73 @@ const Section1 = () => {
     //     [0, 0.1],
     //     ["calc(45vh - 45px)", "calc(0vh - 0px)"]
     // )
-    const top = useTransform(scrollYProgress, [0, 0.1], [50, 1])
+    const progress = useTransform(scrollYProgress, [0, 0.1], [0, 100])
     const scale = useTransform(scrollYProgress, [0, 0.1], [1, 0.4])
     const rotate = useTransform(scrollYProgress, [0, 0.1], [0, 180])
 
-    const variants = {
+    let variants = {
         hidden: {
-            scale: 1.2,
+            // scale: 1.2,
             opacity: 0,
             transition: {
                 duration: 1,
             },
         },
         visible: {
-            scale: 1,
+            // scale: 1,
             opacity: 1,
             transition: {
                 duration: 1,
             },
         },
     }
+    const variants2 = {
+        hidden: {
+            scale: 1.2,
+            opacity: 0,
+        },
+        visible: {
+            scale: 1,
+            opacity: 1,
+        },
+    }
+    // const variants3 = {
+    //     hidden: {
+    //         opacity: 0,
+    //     },
+    //     visible: {
+    //         opacity: 1,
+    //     },
+    // }
 
     const [state, setState] = useState(false)
 
     useEffect(() => {
-        y.onChange(() => {
-            y.get() > -100 ? setState(false) : setState(true)
+        console.log(variants)
+
+        progress.onChange(() => {
+            progress.get() >= 100 ? setState(true) : setState(false)
         })
+    }, [])
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
     }, [])
 
     return (
         <div className="section__one">
+            <AnimatePresence>
+                {state && (
+                    <motion.div
+                        variants={variants2}
+                        initial={"hidden"}
+                        animate={state && "visible"}
+                        exit={"hidden"}
+                        className="navbar"
+                    ></motion.div>
+                )}
+            </AnimatePresence>
+
             <motion.img
                 src={logo}
                 alt=""
@@ -66,6 +105,7 @@ const Section1 = () => {
                 animate={"visible"}
                 className="deathspace_logo"
             />
+            <Particles width="100%" height="200vh" params={particlesJSON} />
         </div>
     )
 }
