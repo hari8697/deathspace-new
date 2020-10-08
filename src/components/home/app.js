265a27684c09a945e6ed6053afcd0bc0bc0874cf
hari8697 <deathspacedesign@gmail.com>
2020-10-08 19:29:44 +0530

    feat: S2 animations

--- src/components/home/app.js
+++ src/components/home/app.js
@@ -1,4 +1,10 @@
 import React from "react"
+import {
+    motion,
+    useViewportScroll,
+    useTransform,
+    AnimatePresence,
+} from "framer-motion"
 
 //static files
 import "../../styles/app.scss"
@@ -8,10 +14,12 @@ import Section1 from "./sections/Section1"
 import Section2 from "./sections/Section2"
 
 const App = () => {
+    const { scrollYProgress } = useViewportScroll()
+
     return (
         <div className="App">
             <Section1></Section1>
-            <Section2></Section2>
+            <Section2 scrollYProgress={scrollYProgress}></Section2>
         </div>
     )
 }
--- src/components/home/sections/Section2.js
+++ src/components/home/sections/Section2.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Particles from "react-particles-js"
 import styled from "styled-components"
 import {
@@ -11,25 +11,89 @@ import {
 //static files
 import particlesJSON from "../../../assets/particles.json"
 
-const Section2 = () => {
-    const Section = styled.div`
+const Section2 = (props) => {
+    const Section = styled(motion.div)`
         position: relative;
-        padding: 30vh 19vw;
+        padding: 30vh 21vw;
     `
 
     const Title = styled(motion.h1)`
-        font-size: 24px;
+        font-size: 1.728rem;
+        line-height: 1.5rem;
         color: #fff;
         opacity: 0.4;
     `
 
     const Para = styled(motion.p)`
         color: #fff;
-        opacity: 0.7;
+        opacity: 0.9;
+        margin-top: 1rem;
     `
 
+    const scrollDistance = [0.1, 0.2]
+    const progress = useTransform(props.scrollYProgress, scrollDistance, [
+        0,
+        100,
+    ])
+    // const opacity = useTransform(props.scrollYProgress, scrollDistance, [0, 1])
+    const y = useTransform(props.scrollYProgress, scrollDistance, [20, 0])
+
+    const [state, setState] = useState(false)
+
+    useEffect(() => {
+        progress.onChange(() => {
+            console.log(progress.get())
+            progress.get() >= 30 ? setState(true) : setState(false)
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    let variants = {
+        hidden: {
+            y: 20,
+            opacity: 0,
+            transition: {
+                duration: 0.5,
+                ease: "easeOut",
+                staggerChildren: 0.5,
+            },
+        },
+        opacityHidden: {
+            opacity: 0,
+            transition: {
+                duration: 0.5,
+                ease: "easeOut",
+            },
+        },
+        opacityVisible: {
+            opacity: 1,
+            transition: {
+                delay: 0.3,
+                duration: 0.7,
+                ease: "easeOut",
+            },
+        },
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+                ease: "easeOut",
+                staggerChildren: 0.5,
+            },
+        },
+        visible2: {
+            y: 0,
+            opacity: 0.4,
+            transition: {
+                duration: 0.5,
+                ease: "easeOut",
+            },
+        },
+    }
     return (
-        <Section>
+        //  <Section>
+        <div style={{ position: "relative", padding: "30vh 21vw" }}>
             <Particles
                 style={{
                     position: "absolute",
@@ -40,15 +104,46 @@ const Section2 = () => {
                 height="100vh"
                 params={particlesJSON}
             />
-            <Title> Design, with purpose.</Title>
-            <Para>
-                Hi! I'm DeathSpace, a front-end web developer & UI/UX enthusiast
-                from Bangalore, India. I specialize in creating websites for
-                businesses that are tailored to their needs, with pixel-perfect
-                code. Oh, and I have a serious passion for making neat
-                animations. Please have a look around & play with the stars!
-            </Para>
-        </Section>
+
+            <AnimatePresence>
+                {state && (
+                    <div>
+                        <Title
+                            variants={variants}
+                            initial={"hidden"}
+                            animate={"visible"}
+                            exit={"hidden"}
+                        >
+                            Design,
+                        </Title>
+
+                        <Title
+                            variants={variants}
+                            initial={"hidden"}
+                            animate={"visible2"}
+                            exit={"hidden"}
+                        >
+                            with purpose.
+                        </Title>
+                        <Para
+                            variants={variants}
+                            initial={"opacityHidden"}
+                            animate={"opacityVisible"}
+                            exit={"opacityHidden"}
+                        >
+                            Hi! I'm DeathSpace, a front-end web developer &
+                            UI/UX enthusiast from Bangalore, India. I specialize
+                            in creating websites for businesses that are
+                            tailored to their needs, with pixel-perfect code.
+                            Oh, and I have a serious passion for making neat
+                            animations. Please have a look around & don't forget
+                            to play with the stars!
+                        </Para>
+                    </div>
+                )}
+            </AnimatePresence>
+        </div>
+        // </Section>
     )
 }
 
