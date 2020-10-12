03b0bbd3350c70f427c6faeebb0531df35f7b8e8
hari8697 <deathspacedesign@gmail.com>
2020-10-12 08:15:00 +0530

    fix: Vercel deploy fix, undo code-splitting

--- src/components/home/app.js
+++ src/components/home/app.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react"
+import React from "react"
 import { useViewportScroll } from "framer-motion"
 import Particles from "react-particles-js"
 // const Particles = lazy(() => import("react-particles-js"))
@@ -11,13 +11,13 @@ import { variants, childVariants } from "../motion/variants"
 //components
 import Section1 from "./sections/Section1"
 import Section2 from "./sections/Section2"
-// const Section2 = lazy(() => import("./sections/Section2"))
-// import Section3 from "./sections/Section3"
+import Section3 from "./sections/Section3"
+import Section4 from "./sections/Section4"
 
-const renderLoader = () => <p>Loading</p>
-const Section3 = lazy(() => import("./sections/Section3"))
-const Section4 = lazy(() => import("./sections/Section4"))
-// import Section4 from "./sections/Section4"
+// const renderLoader = () => <p>Loading</p>
+// const Section2 = lazy(() => import("./sections/Section2"))
+// const Section3 = lazy(() => import("./sections/Section3"))
+// const Section4 = lazy(() => import("./sections/Section4"))
 // const Section4 = lazy(() => import("./sections/Section4"))
 
 const App = () => {
