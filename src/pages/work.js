7d375fb70234dd666b1872b57315913021786ab0
hari8697 <deathspacedesign@gmail.com>
2020-10-24 16:00:15 +0530

    fix: deploy bug

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,5 +1,5 @@
 import React from "react"
-import App from "../components/portfolio/app"
+import App from "../components/portfolio/App"
 import { Helmet } from "react-helmet"
 
 export default function Index() {
